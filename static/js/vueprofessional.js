636dba8f257f166c9ee31f8e86d160af28c970a0
alvarojbarros <alvarojbarros@gmail.com>
2017-04-18 23:43:38 -0300

    commit

--- static/js/vueprofessional.js
+++ static/js/vueprofessional.js
@@ -1,14 +1,5 @@
 Vue.config.devtools = true;
 
-var vue_title = new Vue({
-  el: '#vue_title',
-  data: {
-    Title: '',
-    companyName: '',
-  },
-
-})
-
 var vue_record = new Vue({
   el: '#recordFields',
   data: {
